d8f001c59c3c3be8abb43739f37e1f0919fdd353
Bahrom <bahram101@mail.ru>
2024-09-14 13:21:16 +0500

    start

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,18 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
-};
+module.exports = function (api) {
+	api.cache(true)
+	return {
+		presets: [
+			'babel-preset-expo',
+			['@babel/preset-react', { runtime: 'automatic' }]
+		],
+		plugins: [
+			[
+				'babel-plugin-root-import',
+				{
+					rootPathSuffix: 'app/',
+					rootPathPrefix: '@/'
+				}
+			]
+		]
+	}
+}
