1f67649f4923e68972a992dd6dde209f4649d461
bahram atahanov <bahram101@AuraITs-MacBook-Pro.local>
2024-09-19 23:55:55 +0500

    fixed bottom menu

--- app/components/ui/layout/bottom-menu/BottomMenu.tsx
+++ app/components/ui/layout/bottom-menu/BottomMenu.tsx
@@ -1,26 +1,32 @@
 import { FC } from 'react'
-import { Text, View } from 'react-native'
-import { TypeNavigate } from './menu.interface'
+import { Platform, Text, View } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
-import { menuItems } from './menu.data'
+
 import MenuItem from './MenuItem'
+import { menuItems } from './menu.data'
+import { TypeNavigate } from './menu.interface'
 
 interface IButtonMenu {
-   nav: TypeNavigate
-   currentRoute?: string
+	nav: TypeNavigate
+	currentRoute?: string
 }
 
-const BottomMenu: FC<IButtonMenu> = (props) => {
+const BottomMenu: FC<IButtonMenu> = props => {
+	const { bottom } = useSafeAreaInsets()
 
-   const { bottom } = useSafeAreaInsets()
+	const isAndroid = Platform.OS === 'android'
+	const isIOS = Platform.OS === 'ios'
 
-   return (
-      <View className='pt-5 px-2 flex-row justify-between items-center w-full border-t border-t-solid border-t-[#bbb] bg-white' style={{ paddingBottom: bottom + 20 }}>
-         {menuItems.map(item => (
-            <MenuItem  key={item.path} item={item} {...props}/>
-         ))}
-      </View>
-   )
+	return (
+		<View
+			className='pt-3 pb-5 px-2 flex-row justify-between items-center w-full border-t border-t-solid border-t-[#bbb] bg-white'
+			style={{ paddingBottom: isIOS ? bottom : isAndroid ? bottom + 12 : 0 }}
+		>
+			{menuItems.map(item => (
+				<MenuItem key={item.path} item={item} {...props} />
+			))}
+		</View>
+	)
 }
 
 export default BottomMenu
