1204fc9aecbc46c0c5f0a5df92cc76356bb40b00
Bahrom <bahram101@mail.ru>
2024-09-25 16:54:28 +0500

    get new data of day from server

--- app/components/screens/front-page/FrontPage.tsx
+++ app/components/screens/front-page/FrontPage.tsx
@@ -1,5 +1,5 @@
-import React, { FC, useEffect, useMemo, useState } from 'react'
-import { Dimensions, Text, View, useWindowDimensions } from 'react-native'
+import React, { FC, useCallback, useState } from 'react'
+import { Dimensions, Text, View } from 'react-native'
 import RenderHTML from 'react-native-render-html'
 import { SwiperFlatList } from 'react-native-swiper-flatlist'
 
@@ -7,18 +7,15 @@ import Layout from '@/components/layout/Layout'
 import { useData } from '@/components/screens/back-page/useData'
 import Loader from '@/components/ui/Loader'
 
-import { convertToHijriMonth, convertToMiladiMonth } from '@/utils/helpers'
-import axios from 'axios'
 
-interface UseDataResponse {
-	data: any
-	isLoading?: boolean
-}
+// interface UseDataResponse {
+// 	data: any
+// 	isLoading?: boolean
+// }
 
 const FrontPage: FC = () => {
 	const { width } = Dimensions.get('window')
-	// const { width } = useWindowDimensions()
-	const [currentData, setCurrentData] = useState(null);
+	const [activeIndex, setActiveIndex] = useState(1);
 	const [isLoading, setIsLoading] = useState(false);
 	const [dataList, setDataList] = useState([
 		{
@@ -32,8 +29,10 @@ const FrontPage: FC = () => {
 			year_month: "Қыркүйек / 2024 жыл",
 			dayofweek: 'Дүйсенбі',
 			day: 23,
+
 		},
 		{
+
 			id: 4365,
 			date: '2024-09-24',
 			history:
@@ -44,8 +43,10 @@ const FrontPage: FC = () => {
 			year_month: "Қыркүйек / 2024 жыл",
 			dayofweek: 'Сейсенбі',
 			day: 24,
+
 		},
 		{
+
 			id: 4366,
 			date: '2024-09-25',
 			history:
@@ -56,47 +57,48 @@ const FrontPage: FC = () => {
 			year_month: "Қыркүйек / 2024 жыл",
 			dayofweek: 'Сәрсенбі',
 			day: 25,
+
 		}
 	])
 
-	// useEffect(() => {
-
-	// }, [])
-
-
 	const fetchData = async (date: string) => {
+		setIsLoading(true);
 		try {
-			setIsLoading(true);
-			const response = await axios.get(`https://kuntizbe.kz/json/datas?date=${date}`);
-			setCurrentData(response.data);
+			const response = await fetch(`https://kuntizbe.kz/json/datas?date=${date}`);
+			const res = await response.json()
+			setDataList(prev => [...prev, res.front])
 		} catch (error) {
-			console.error('Error fetching data', error);
+			setIsLoading(false);
 		} finally {
 			setIsLoading(false);
 		}
 	};
 
-
-
-	const handleChange = ({ index }: { index: number }) => {
-		const currentSlide = dataList[index]
-		console.log('INDEX', index)
-		console.log('currentSlide',currentSlide)
-
-		// // console.log('CURRENT_SLIDE', currentSlide)
-		// if (currentSlide && currentSlide.date) {
-		// 	fetchData(currentSlide.date);
-		// }
+	const getNextDate = (date: string): string => {
+		const currentDate = new Date(date)
+		currentDate.setDate(currentDate.getDate() + 1)
+		return currentDate.toISOString().split('T')[0].toString()
 	}
 
-	// console.log('currentData', currentData)
+	const handleChange = useCallback(({ index }: { index: number }) => {
+		setActiveIndex(index);
+
+		if (index === dataList.length - 1 && !isLoading) {
+			const lastItem = dataList[dataList.length - 1];
+			const nextDate = getNextDate(lastItem.date);
+			fetchData(nextDate);
+		}
+	}, [dataList, isLoading]);
+
+	console.log('DATA_LIST', JSON.stringify(dataList, null, 2));
 
 	return <Layout>
 		<SwiperFlatList
-			onChangeIndex={(data)=>handleChange(data)}
+			onChangeIndex={(data) => handleChange(data)}
 			data={dataList}
-			renderItem={({ item }) => (
-				<View className={`items-center pt-8 px-5`} style={[{ width }]}>
+			index={activeIndex}
+			renderItem={({ item }) => {
+				return <View className={`items-center pt-8 px-5`} style={[{ width }]}>
 					<Text className='text-xl font-bold uppercase selft-start'>
 						{item.hijri_date}
 					</Text>
@@ -109,15 +111,16 @@ const FrontPage: FC = () => {
 						{item.year_month}
 					</Text>
 
-					<RenderHTML contentWidth={width} source={{ html: item.history }} />
+					<Text style={{ width }}>
+						{item.history}
+					</Text>
 
-					<RenderHTML
-						contentWidth={width}
-						source={{ html: item.quote }}
-					// tagsStyles={{ p: { textAlign: 'center' } }}
-					/>
+					<Text style={{ width }}>
+						{item.quote}
+					</Text>
 				</View>
-			)}
+
+			}}
 		/>
 	</Layout>
 
